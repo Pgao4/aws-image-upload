d894256856fa003363f2a710afdf64dcae6353e1
pgao4 <pgao4@ualberta.ca>
2022-02-07 21:47:00 -0700

    Initial commit

---
+++ src/main/java/com/feicode/awsimageupload/datastore/FakeUserProfileDataStore.java
@@ -0,0 +1,19 @@
+package com.feicode.awsimageupload.datastore;
+
+import com.feicode.awsimageupload.profile.UserProfile;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class FakeUserProfileDataStore {
+
+    private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
+
+    static {
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "janetjones",null));
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "antoniojunior",null));
+    }
+}
