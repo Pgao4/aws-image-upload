68d4db2eabb060d190273649ee522e97f87b7bd9
pgao4 <pgao4@ualberta.ca>
2022-02-08 23:16:16 -0700

    fix

--- src/main/java/com/feicode/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/feicode/awsimageupload/config/AmazonConfig.java
@@ -16,8 +16,7 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIA2XQULZGHXDKUKUJP",
-                "NEtxBZaDh8G70h6BFL7CiQVpWAwoiZlg8CYqrdAO"
+
         );
         return AmazonS3Client
                 .builder()
